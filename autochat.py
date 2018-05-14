67f98b7f60f7252c470bad0e2fecbfda28ec1b94
Magician <chenhao0503@gmail.com>
2018-05-14 09:23:51 +0800

    Update autochat.py

--- autochat.py
+++ autochat.py
@@ -3,7 +3,9 @@ import requests
 import json
 import itchat
 
-itchat.auto_login(enableCmdQR=2)
+groups = []
+
+itchat.auto_login(hotReload=True,enableCmdQR=2)
 # 调用图灵机器人的api，采用爬虫的原理，根据聊天消息返回回复内容
 def tuling(info):
     appkey = "e5ccc9c7c8834ec3b08940e290ff1559"
@@ -19,22 +21,36 @@ def group_id(name):
     df = itchat.search_chatrooms(name=name)
     return df[0]['UserName']
 
+groups.append(group_id(u'库有引力'))
+groups.append(group_id(u'大北湖幸福快乐的家'))
+groups.append(group_id(u'早睡早起方能养生'))
+#groups.append(group_id(u'小伙伴们'))
+
+mygirl = itchat.search_friends(name=u'我方水晶')[0]['UserName']
+
 # 注册文本消息，绑定到text_reply处理函数
 # text_reply msg_files可以处理好友之间的聊天回复
 @itchat.msg_register([TEXT,MAP,CARD,NOTE,SHARING])
 def text_reply(msg):
-    itchat.send('%s' % tuling(msg['Text']),msg['FromUserName'])
+    if msg['FromUserName'] == mygirl:
+        if u'老公' in msg['Text']:
+            itchat.send(u'媳妇，有何吩咐！', mygirl)
+    else:
+        friend = itchat.search_friends(userName=msg['FromUserName'])
+        friend_name = friend['RemarkName']
+        print('%s said: %s' % (friend_name, msg['Text']))
+        itchat.send('%s' % tuling(msg['Text']),msg['FromUserName'])
 
+'''
 @itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
 def download_files(msg):
     msg['Text'](msg['FileName'])
     return '@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName'])
-
+'''
 @itchat.msg_register([TEXT], isGroupChat=True)
 def group_text_reply(msg):
-    # 当然如果只想针对@你的人才回复，可以设置if msg['isAt']: 
-    item = group_id(u'首航公益互助交流群')
-    if msg['FromUserName'] == item:
+    item = msg['FromUserName']
+    if item in groups:
         itchat.send(u'%s' % tuling(msg['Text']), item)
 
 itchat.run()
